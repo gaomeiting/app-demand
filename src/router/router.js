2345e4f5e55e4fc3f74a8c9758b11578ef8b523c
潘芸燕 <402015534@qq.com>
2018-10-22 15:01:52 +0800

    menu

--- src/router/router.js
+++ src/router/router.js
@@ -43,42 +43,22 @@ const routes = [
         {
           path: '/order',
           name: 'order',
-          meta: {breadcrumbName: '订单中心', requireAuth: true},
-          component: RouteView,
-          children: [
-            {
-              path: '/order/fast',
-              name: 'order_fast',
-              meta: {breadcrumbName: '快捷订单', requireAuth: true},
-              component: () => import("@/views/order/fast.vue")
-            },
-            {
-              path: '/order/:id',
-              name: 'order_fast_detail',
-              meta: {breadcrumbName: '快捷订单详情', requireAuth: true},
-              component: () => import("@/views/order/fastDetail.vue")
-            },
-            {
-              path: '/order/project',
-              name: 'order_project',
-              meta: {breadcrumbName: '项目订单', requireAuth: true},
-              component: () => import("@/views/order/project.vue")
-            },
-          ]
+          meta: {breadcrumbName: '我的订单', requireAuth: true},
+          component: () => import("@/views/order/index.vue"),
+        },
+        {
+          path: '/order/detail',
+          name: 'order_detail',
+          meta: {breadcrumbName: '订单详情', requireAuth: true},
+          component: () => import("@/views/order/detail.vue"),
         },
         {
           path: '/demands',
           name: 'demands',
           meta: {breadcrumbName: '需求广场', requireAuth: true},
-          redirect: '/demands/index',
+          redirect: '/demands/fast',
           component: RouteView,
           children: [
-            {
-              path: '/demands/index',
-              name: 'demands_index',
-              meta: {breadcrumbName: '试音需求', requireAuth: true},
-              component: () => import('@/views/demands/index.vue')
-            },
             {
               path: '/demands/fast',
               name: 'demands_fast',
@@ -97,49 +77,7 @@ const routes = [
           path: '/wallet',
           name: 'wallet',
           meta: {breadcrumbName: '我的钱包', requireAuth: true},
-          redirect: '/wallet/index',
-          component: RouteView,
-          children: [
-            {
-              path: '/wallet/index',
-              name: 'wallet_index',
-              meta: {breadcrumbName: '结算中心', requireAuth: true},
-              component: () => import('@/views/wallet/index.vue')
-            },
-            {
-              path: '/wallet/withdraw',
-              name: 'wallet_withdraw',
-              meta: {breadcrumbName: '提现管理', requireAuth: true},
-              component: () => import('@/views/wallet/withdraw.vue')
-            },
-            {
-              path: '/wallet/income',
-              name: 'wallet_income',
-              meta: {breadcrumbName: '收支明细', requireAuth: true},
-              component: () => import('@/views/wallet/income.vue')
-            },
-          ]
-        },
-        {
-          path: '/setting',
-          name: 'setting',
-          meta: {breadcrumbName: '账号设置', requireAuth: true},
-          redirect: '/setting/user',
-          component: RouteView,
-          children: [
-            {
-              path: '/setting/user',
-              name: 'setting_user',
-              meta: {breadcrumbName: '个人信息', requireAuth: true},
-              component: () => import('@/views/setting/user.vue')
-            },
-            {
-              path: '/setting/safety',
-              name: 'setting_safety',
-              meta: {breadcrumbName: '账户安全', requireAuth: true},
-              component: () => import('@/views/setting/safety.vue')
-            },
-          ]
+          component:() => import('@/views/wallet/index.vue')
         },
       ]
     },
