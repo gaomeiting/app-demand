2e886e002e705e47b2b74207a0394c0923e6f2dc
潘芸燕 <402015534@qq.com>
2018-10-22 15:02:04 +0800

    menu

---
+++ src/views/order/detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <page-layout title="订单详情">
+    <a-card title="待开发项目"></a-card>
+  </page-layout>
+</template>
+
+<script>
+  import PageLayout from '@/layout/PageLayout'
+  export default {
+    components: {PageLayout},
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
