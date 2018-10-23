a221304de5ab64aeecb4d4d11e2ad35f001f253a
gaomeiting <1959151877@qq.com>
2018-10-23 19:24:26 +0800

    feat: 工作台修改

--- src/views/task/task.vue
+++ src/views/task/task.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
 <task-item @goTaskDetail="goTaskDetail"></task-item>
+
+<figure @click="goDemand">
+  <img src="/publicImages/banner.jpg">
+</figure>
 </div>
 </template>
 
@@ -12,6 +16,9 @@ export default {
       this.$router.push({
   				path: `/index/123`
   		})
+    },
+    goDemand() {
+      this.$router.push('/demands/fast')
     }
   },
   components: {
@@ -21,4 +28,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+img {
+  width: 100%;
+}
 </style>
